e642850df261522e06a1b657785d8dd95ffdc718
Numan Naeem <numannaeem134@gmail.com>
2026-06-30 23:33:58 +0500

    Cleanup: Remove redundant scratch, test, and patch scripts from repository

--- server/scratch/check_retail_data.js
+++
@@ -1,2 +0,0 @@
-const { Pool } = require('pg');
-// Let's query using the debug endpoint. We will add a temporary route to get summary counts of products, customers, sales, bank accounts grouped by module_type and user_id!
--- server/scratch/sync_local_db.js
+++
@@ -1,8 +0,0 @@
-delete process.env.DATABASE_URL;
-require('../api/utils/dbInit')().then(() => {
-  console.log('Local DB sync complete!');
-  process.exit(0);
-}).catch(err => {
-  console.error(err);
-  process.exit(1);
-});
